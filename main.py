329fe98a53c49bb2411c4e537ff342784ca427ed
RandawLucas <r.randawlucas@hotmail.com>
2021-09-26 23:02:04 -0300

    add try/except in create() and Inserir()

--- main.py
+++ main.py
@@ -3,32 +3,50 @@ from datetime import date;
 import sqlite3;
 import matplotlib.pyplot as grafico;
 
-connect = sqlite3.connect('D:\projetos_estudos\Desafios-Python\Desafios-Python3\sqlite3\SQLiteDados.db')
+connect = sqlite3.connect('sqlite3\SQLiteDados.db')
 command = connect.cursor()
 
 request = requests.get('https://api.hgbrasil.com/finance?format=json-cors&key=bbd4b1d1')
 resposta = request.json()
 data_requsicao = date.today()
-data_format = '{}-{}-{}'.format(data_requsicao.day, data_requsicao.month, data_requsicao.year)
+data_format = '{}/{}/{}'.format(data_requsicao.day, data_requsicao.month, data_requsicao.year)
 dolar = resposta['results']['currencies']['USD']['buy']
 euro = resposta['results']['currencies']['EUR']['buy']
-def inserir():
-    command.execute("""INSERT INTO moedas (Data, Dolar, Euro) VALUES(?,?,?)""", (data_format,dolar, euro))
-    connect.commit()
-    print('Dados inseridos com sucesso!')
 
 tempo = []
 valorDolar = []
 valorEuro = []
+
+def create():
+
+    try:
+        command.execute("""
+            CREATE TABLE moedas(
+            Data Date NOT NULL PRIMARY KEY,
+            Dolar Float NOT NULL,
+            Euro Float NOT NULL        
+            );""")
+        print ('Tabela Criada Com Sucesso!')
+    except:
+        print('Conectado ao banco de dados')
+
+def inserir():
+    try:
+        command.execute("""INSERT INTO moedas (Data, Dolar, Euro) VALUES(?,?,?)""", (data_format, dolar, euro))
+        connect.commit()
+        print('Dados inseridos com sucesso!')
+    except:
+        print('O Banco de dados não aceita Duplicatas')
+
 def exibir():
 
-    command.execute("""SELECT * FROM moedas""")
+    command.execute("""SELECT * FROM moedas ORDER BY Data""")
     for linha in command.fetchall():
         tempo.append(linha[0])
         valorDolar.append(linha[1])
         valorEuro.append(linha[2])
-        #print(linha)
-        connect.close()
+        print(linha)
+        #connect.close()
 
     print()
 #Adicionando grafico Visual da blibioteca matplotlib (mas utilizei somente o py plot, ficou: "matplotlib.pyplot")
@@ -47,10 +65,12 @@ def graficos():
     grafico.show() #Exibe o gráfico
 
 if request.status_code == 200:
-    print('\nValores da cotação de hoje: ',data_format,'\nDolar: {:.2f}'.format(dolar),'\nEuro: {:.2f}'.format(euro))
+    print('\nValores da cotação de hoje: ',data_format,'\nDolar: {:.2f}'.format(dolar),'\nEuro: {:.2f}'.format(euro)) #Exibe a Cotação do Dia referente a API
     print('')
-    #inserir()
-    exibir()
-    graficos()
+    create()
+    inserir() #Insere A cotação do dia da APi ao Banco de dados Local
+    exibir() #Exibe os dados do Banco de dados Local
+    graficos() #Gera Um Gráfico com o Banco de Dados Local
+    connect.close() #Fecha a conecção com o Banco de Dados
 else:
     print('erro de requisição')
